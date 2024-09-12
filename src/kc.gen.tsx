08d782cb707596f727fa7eb21b0e3c13582a5da2
Arne Schulz <arne.schulz@otto.de>
2024-09-12 12:02:28 +0200

    upgrade storybook and add mui components

--- src/kc.gen.tsx
+++ src/kc.gen.tsx
@@ -10,9 +10,9 @@
 
 import { lazy, Suspense, type ReactNode } from "react";
 
-export type ThemeName = "keycloakify-starter";
+export type ThemeName = "skylab";
 
-export const themeNames: ThemeName[] = ["keycloakify-starter"];
+export const themeNames: ThemeName[] = ["skylab"];
 
 export type KcEnvName = never;
 
